d97c32ba132f2cf4e1eecfa6e2d8d8c4d3d629b7
Christopher <114704469+Blade-24@users.noreply.github.com>
2023-06-28 22:05:08 -0600

    clean code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // !proyecto chat online
+"use strict";
+
 const time = () => {
   const ObjetDate = new Date()
   const [hour, minutes] = [
@@ -10,11 +12,58 @@ const time = () => {
 }
 
 const container = document.querySelector(".container")
+const chatContainer = document.querySelector(".chat-container")
 const contentChat = document.querySelector(".chat-content");
 
+const createNewMsg = () => {
+  let inputMsg = document.querySelector("input");
+
+  if (inputMsg.value.length >= 1) {
+    const msgContent = document.createElement("div");
+    const msgText = document.createElement("div");
+    const msgHour = document.createElement("div");
+
+    msgContent.classList.add("msg");
+    msgText.classList.add("msg-text");
+    msgHour.classList.add("msg-hour");
+
+    msgText.textContent = inputMsg.value;
+    msgHour.textContent = time();
+
+    msgContent.appendChild(msgText);
+    msgContent.appendChild(msgHour);
+    contentChat.appendChild(msgContent);
+  } else {
+    inputMsg.setAttribute("placeholder", "Minimo una letra...")
+    inputMsg.classList.add("animation-text");
+    setTimeout(() => {
+      inputMsg.classList.remove("animation-text");
+      inputMsg.setAttribute("placeholder", "Escribe tu mensaje...");
+    }, 2500)
+  }
+
+  inputMsg.value = "";
+}
+
 const htmlCodeStruct = () => {
+  const navegationChat = document.querySelector(".navegation");
+  const navegationContent = document.createElement("div");
+
+  navegationContent.classList.add("navegation-content");
+  const chatName = document.createElement("div");
+  const barMenu = document.createElement("i");
+
+  chatName.setAttribute("contenteditable", "true");
+  chatName.innerHTML = "<h2>Agregar</h2>"
+  barMenu.classList.add("bi");
+  barMenu.classList.add("bi-list");
+
+  navegationContent.appendChild(chatName);
+  navegationContent.appendChild(barMenu);
+  navegationChat.appendChild(navegationContent);
+
   const typeSend = document.createElement("div");
-  let inputMsg = document.createElement("input");
+  const inputMsg = document.createElement("input");
   const sendButton = document.createElement("button");
 
   typeSend.classList.add("type-send");
@@ -28,65 +77,12 @@ const htmlCodeStruct = () => {
   contentChat.appendChild(typeSend);
   container.appendChild(typeSend);
 
-  sendButton.addEventListener("click", () => {
-    if (inputMsg.value.length >= 1) {
-      const msgContent = document.createElement("div");
-      const msgText = document.createElement("div");
-      const msgHour = document.createElement("div");
+  sendButton.addEventListener("click", createNewMsg);
 
-      msgContent.classList.add("msg");
-      msgText.classList.add("msg-text");
-      msgHour.classList.add("msg-hour");
-
-      msgText.textContent = inputMsg.value;
-      msgHour.textContent = time();
-
-      msgContent.appendChild(msgText);
-      msgContent.appendChild(msgHour);
-      contentChat.appendChild(msgContent);
-    } else {
-      inputMsg.setAttribute("placeholder", "Minimo una letra...")
-      inputMsg.classList.add("animation-text");
-      setTimeout(() => {
-        inputMsg.classList.remove("animation-text");
-        inputMsg.setAttribute("placeholder", "Escribe tu mensaje...");
-      }, 2500)
-    }
+  inputMsg.addEventListener("keypress", (e) => {
+    const keyPress = e.key;
+    if (keyPress == "Enter") createNewMsg();
   })
-
-  inputMsg.addEventListener("keyup", keyPressEnter)
-  function keyPressEnter(event) {
-    const precionDeTecla = event.key;
-    if (precionDeTecla == "Enter") {
-      if (inputMsg.value.length >= 1) {
-        const msgContent = document.createElement("div");
-        const msgText = document.createElement("div");
-        const msgHour = document.createElement("div");
-
-        msgContent.classList.add("msg");
-        msgText.classList.add("msg-text");
-        msgHour.classList.add("msg-hour");
-
-        msgText.textContent = inputMsg.value;
-        msgHour.textContent = time();
-
-        msgContent.appendChild(msgText);
-        msgContent.appendChild(msgHour);
-        contentChat.appendChild(msgContent);
-      }
-    } else {
-      inputMsg.setAttribute("placeholder", "Minimo una letra...")
-      inputMsg.classList.add("animation-text");
-      setTimeout(() => {
-        inputMsg.classList.remove("animation-text");
-        inputMsg.setAttribute("placeholder", "Escribe tu mensaje...");
-      }, 2500)
-    }
-
-  }
-
-  inputMsg.value = "";
-  // return msgContent;
 }
 
-htmlCodeStruct()
+htmlCodeStruct();
